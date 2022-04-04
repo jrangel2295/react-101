4e9e9a7ebdfbe3f77f61507394661164633a5589
Johnny Rangel <jrangel2295@gmail.com>
2022-04-04 17:30:30 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+import'./App.css';
+
+function App() {
+  const [count, setCount] = useState(0);
+  return (
+    <div>
+    <h1> {count}</h1>
+    <button onClick={()=> setCount(count + 1)}>Add</button>
+    <button onClick={()=> setCount(count - 1)}>Subtract</button>
+    <button onClick={()=> setCount(0)}>Reset</button>    
+    </div>
+  );
+}
+
+export default App;
